7e31ce61a5664583a993191276e26e938208079b
Michelle Davis <mdavis1999@hotmail.com>
2016-07-25 14:26:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<h1>Hello OpenShift 3.1!</h1>'; ?> 
+ <?php echo '<h1>Hello OpenShift 3.1! . $_SERVER['SERVER_ADDR'] </h1>'; ?> 
  </body>
 </html>
