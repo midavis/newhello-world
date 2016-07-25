f2040745cd5714e618b165319cccc8a7a458c0ec
Michelle Davis <mdavis1999@hotmail.com>
2016-07-25 14:42:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
-echo "Hello from Michelles Big Pod " . $_SERVER['SERVER_ADDR'];
+<?php echo '<h1>Hello OpenShift 3.1!</h1>' $_SERVER['SERVER_ADDR']; ?> 
  </body>
 </html>
