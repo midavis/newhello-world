7d794a760151f971fbb97256648dbfa9828d664b
Michelle Davis <mdavis1999@hotmail.com>
2017-12-04 15:16:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
-<?php echo "Hello OpenShift from Michelles at Scaled Jenkins Pod with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
+<?php echo "Hello OpenShift from Michelles at Scaled UP Jenkins Pod with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
  </body>
 </html>
