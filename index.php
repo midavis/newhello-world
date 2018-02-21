bf6e36bc8e89e130ea145bc66de79824f3262b96
Michelle Davis <mdavis1999@hotmail.com>
2018-02-21 13:27:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>PHP HW TEST</title>
  </head>
  <body>
 <?php echo "Hello OpenShift from Michelle at Jenkins Pod with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
