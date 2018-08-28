c8ad003acffdad04c45c66f7ea5dc62f31f87ceb
Michelle Davis <mdavis1999@hotmail.com>
2018-08-28 13:47:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP HW TEST</title>
  </head>
  <body>
-<?php echo "Hello OpenShift from Michelle new at Pod with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
+<?php echo "Hello OpenShift from Michelle dwerwe at Pod with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
  </body>
 </html>
