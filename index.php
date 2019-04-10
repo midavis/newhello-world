754feddb336242e35a38893fe4ce78fdce306683
Michelle Davis <mdavis1999@hotmail.com>
2019-04-10 15:02:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP HW TEST</title>
  </head>
  <body>
-<?php echo "Hello OpenShift from NEWER Michelle with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
+<?php echo "Hello OpenShift from Pipelined Michelle with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
  </body>
 </html>
