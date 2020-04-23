18127bf61eefae89327a2b623e5b68ab71948597
Michelle Davis <mdavis1999@hotmail.com>
2020-04-23 13:51:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP HW TEST</title>
  </head>
  <body>
-<?php echo "Hello OpenShift from Michelle with IP Address old CODE " , $_SERVER['SERVER_ADDR']; ?> 
+<?php echo "Hello OpenShift from Michelle with IP Address " , $_SERVER['SERVER_ADDR']; ?> 
  </body>
 </html>
